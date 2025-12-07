70a0ded5ded519d2ee8af919d91e960a48cb57ce
yasserbawzier <yasser.bawzier@gmail.com>
2025-12-07 18:07:14 +0300

    Delete output(Print) directory

--- output(Print)/output(Print)/output(Print).cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-using namespace std;
-int main()
-{
-   cout << "Hello World!\n";
-   cout << "i am learning c++ programming language\n";
-   cout << "i was bilding many projects in c++ language\nbut now i will all building projects agin\n and downlowd to get hup account";
-}
