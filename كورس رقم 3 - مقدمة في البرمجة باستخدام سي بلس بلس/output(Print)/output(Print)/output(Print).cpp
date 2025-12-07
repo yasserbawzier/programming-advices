a472b67f8e236fa6720e84678850a89f80a8d102
yasserbawzier <yasser.bawzier@gmail.com>
2025-12-07 18:10:52 +0300

    كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس
    in this file i will building all exercises the course "كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس"

---
+++ كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس/output(Print)/output(Print)/output(Print).cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout << "Hello World!\n";
+   cout << "i am learning c++ programming language\n";
+   cout << "i was bilding many projects in c++ language\nbut now i will all building projects agin\nand downlowd to get hup account";
+}
