6829760ff401504a7d663d0983baed51319b0c24
yasserbawzier <yasser.bawzier@gmail.com>
2025-12-08 07:59:02 +0300

    Add files via upload

---
+++ كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس/Literals and Escape sequences/Literals and Escape sequences/Literals and Escape sequences.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "\a";
+	cout << "Dear Sir\\Modom,\n";
+	cout << "How are you ?\n";
+	cout << "My name is \"Mohammed\" ,nice to meet you.\n";
+	cout << "Ali\tAhmed\tLina\nFadi\tZain\tMona" << endl;
+}
