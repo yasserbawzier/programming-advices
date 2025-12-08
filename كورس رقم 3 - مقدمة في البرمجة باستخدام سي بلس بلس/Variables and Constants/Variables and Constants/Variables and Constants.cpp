a16efc436a555e52035f57cd9f15d819f0439952
yasserbawzier <yasser.bawzier@gmail.com>
2025-12-08 10:06:46 +0300

    variables and Constants
    in this file you will learn the difference between variables and constants

---
+++ كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس/Variables and Constants/Variables and Constants/Variables and Constants.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main() {
+	//If you want to create any variables, you must name them with names that explain their function
+	//ÅĞÇ ßäÊ ÊÑíÏ ÅäÔÇÁ Ãí ãÊÛíÑÇÊ¡ íÌÈ Úáíß ÊÓãíÊåÇ ÈÃÓãÇÁ ÊÔÑÍ æÙíİÊåÇ
+	const int Year = 12;
+	string Name = "Yasser Mohammed Ail Bawzier";
+	short Age = 22;
+	string City = "Mokla";
+	string Country = "Yamen";
+	float Monthly_Salary = 5000;
+	int Yearly_Salary =Monthly_Salary*Year;
+	char Gender = 'M';
+	bool isMarrid = true;
+
+
+	cout << "******************************\n";
+	cout << "Name:"<<Name<<"\n";
+	cout << "Age :"<<Age<<"Years.\n";
+	cout << "City:" <<City<<"\n";
+	cout << "Country:"<<Country<<"\n";
+	cout << "Monthly Salary :"<<Monthly_Salary<<"\n";
+	cout << "Yearly Salary: " <<Yearly_Salary<< endl;
+	cout << "Gender: "<<Gender<<"\n";
+	cout << "Married : "<<Marrid<<"\n";
+	cout << "******************************\n";
+}
