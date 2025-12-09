43602a09e09200ca4a6b5c1dc47a5a8fac698b2d
yasserbawzier <yasser.bawzier@gmail.com>
2025-12-09 10:36:56 +0300

    Add files via upload

---
+++ كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس/Assignment Operators/Assignment Operators/Assignment Operators.cpp
@@ -0,0 +1,29 @@
+﻿//Assignment Operators in C++ are used to assign values to variables.
+//Assignment operators it is like =, +=, -=, *=, /=, %=, <<=, >>=, &=, ^=, |= etc.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A = 10, B=20;
+
+	A += B;// A = A + B
+	cout << "A = " << A << endl;
+
+
+	A -= B;// A = A - B
+	cout << "A = " << A << endl;
+
+
+
+	A *= B;// A = A * B
+	cout << "A = " << A << endl;
+
+
+
+	A /= B;// A = A / B
+	cout << "A = " << A << endl;
+
+	A %= B;// A = A % B
+	cout << "A = " << A << endl;
+
+}
