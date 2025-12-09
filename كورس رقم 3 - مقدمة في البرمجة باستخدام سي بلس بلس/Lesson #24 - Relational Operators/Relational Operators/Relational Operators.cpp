e9bbef2daeb6e3dd65963c8d65004286604169f4
yasserbawzier <yasser.bawzier@gmail.com>
2025-12-09 10:51:43 +0300

    Relational Operators it is compare two values
    Relational Operators like (==,!=,<=,>=,<,>)

---
+++ كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس/Lesson #24 - Relational Operators/Relational Operators/Relational Operators.cpp
@@ -0,0 +1,19 @@
+﻿//(Compare (مقارنة)),(Relational Operators (العوامل العلائقية))
+//the Relational Operators in C++ are used to compare two values 
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A = 10, B = 20;
+
+	cout << (A == B) << endl;
+	cout << (A != B) << endl;
+	cout << (A > B) << endl;
+	cout << (A < B) << endl;
+	cout << (A >= B) << endl;
+	cout << (A <= B) << endl;
+	return 0;
+
+
+
+}
