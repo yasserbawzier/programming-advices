fd769171fbb8eb9c946cda737cf236224cb3f348
yasserbawzier <yasser.bawzier@gmail.com>
2025-12-09 10:34:40 +0300

    Add files via upload

---
+++ كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس/Postfix vs prefix__++A vs A++___--A vs A--/Postfix vs prefix__++A vs A++___--A vs A--/Postfix vs prefix__++A vs A++___--A vs A--.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	//(increase)سيزيد
+	int A = 10;
+	int B = A++;//B will tke the old value of A, then A will increase by 1
+
+	cout << "B = " << B << endl;
+	cout << "A = " << A << endl;
+
+	B = ++A;//A will increase by 1 , then B will take the new value of A
+	
+	cout << "B = " << B << endl;
+	cout << "A = " << A << endl;
+
+
+}
