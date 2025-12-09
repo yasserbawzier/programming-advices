a977e9e2607b63695f0eefa7ab927fbca149074e
yasserbawzier <yasser.bawzier@gmail.com>
2025-12-09 11:03:24 +0300

    Logical Operators it is (&& , || . !)

---
+++ كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس/Lesson #25 - Logical Operators/Lesson #25 - Logical Operators/Lesson #25 - Logical Operators.cpp
@@ -0,0 +1,15 @@
+﻿// Lesson #25 - Logical Operators
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	bool A = true, B = false;
+	cout << (A && B) << endl;
+	cout << (A || B) << endl;
+	cout << (!A) << endl;
+	cout << (!B) << endl;
+	cout << !(A && B) << endl;
+	cout << !(A || B) << endl;
+	return 0;
+}
