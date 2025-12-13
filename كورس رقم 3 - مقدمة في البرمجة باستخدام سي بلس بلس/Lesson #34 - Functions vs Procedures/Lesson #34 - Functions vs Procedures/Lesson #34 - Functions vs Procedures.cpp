10882baed5834adbcdbddd5d1253ea4d64069314
yasserbawzier <yasser.bawzier@gmail.com>
2025-12-13 10:05:54 +0300

    Add files via upload

--- كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس/Lesson #34 - Functions vs Procedures/Lesson #34 - Functions vs Procedures/Lesson #34 - Functions vs Procedures.cpp
+++ كورس رقم 3 - مقدمة في البرمجة باستخدام سي بلس بلس/Lesson #34 - Functions vs Procedures/Lesson #34 - Functions vs Procedures/Lesson #34 - Functions vs Procedures.cpp
@@ -65,6 +65,38 @@ void SwapNamber(int& Numbe1, int& Numbe2) {
 	Numbe1 = Numbe2;
 	Numbe2 = temp;
 }
+int secondsInHours(short hours) {
+	return hours * 3600;
+}
+short secondsInMinutes(short minutes) {
+	return minutes * 60;
+}
+int SecondsInDays(short days) {
+		return days * 24 * 3600;
+
+}
+int TotalSecondsOfWork(short days, short hours, short minutes, short seconds) {
+	return SecondsInDays(days) + secondsInHours(hours) + secondsInMinutes(minutes) + seconds;
+}
+int ConvertSecodsToDay(int Seconds) {
+	return Seconds / (24 * 60 * 60);
+}
+int ConvertSecodsToHours(int Seconds) {
+	return Seconds / (60*60);
+}
+int ConvertSecondsToMinuts(int Seconds) {
+	return Seconds / 60;
+}
+void TotalSecondsOfDays_Hours_minutes(int seconds) {
+	short days = ConvertSecodsToDay(seconds);
+	seconds -= SecondsInDays(days);
+	short Hours = ConvertSecodsToHours(seconds);
+	seconds -= secondsInHours(Hours);
+	short minuts = ConvertSecondsToMinuts(seconds);
+	seconds -= secondsInMinutes(minuts);
+	cout << days << ":" << Hours << ":" << minuts << ":" << seconds << endl;
+
+}
 int main()
 {
 	//this is the proplem hire a drive - case 1
@@ -106,6 +138,27 @@ int main()
 	cout << "Number 1 after swap is :" << Number1 << endl;
 	cout << "Number 2 after swap is :" << Number2 << endl;
 	*/
+
+	//this is the proplem #42 - Task Duration in Seconds
+
+	/*short days, hours, minutes, seconds;
+	cout << "please enter days:";
+	cin >> days;
+	cout << "Please enter hours:";
+	cin >> hours;
+	cout << "please enter minutes:";
+	cin >> minutes;
+	cout << "please enter seconds:";
+	cin >> seconds;
+	system("cls");
+	cout<<"the Total Seconds of Werk is :"<<TotalSecondsOfWork(days, hours, minutes, seconds);
+	*/
+
+	//this is Problem #43 - Seconds to days hours minutes seconds
+	/*
+	TotalSecondsOfDays_Hours_minutes(10000);
+	*/
+	
 	return 0;
 
 
