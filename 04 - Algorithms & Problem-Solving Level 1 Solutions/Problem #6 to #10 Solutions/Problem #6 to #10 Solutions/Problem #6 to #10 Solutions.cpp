df113017164a2d2c4254c75d8d16a87ca7a30f4b
yasserbawzier <yasser.bawzier@gmail.com>
2025-12-17 08:56:59 +0300

    Add Problem #11 to #15 Solution

--- 04 - Algorithms & Problem-Solving Level 1 Solutions/Problem #6 to #10 Solutions/Problem #6 to #10 Solutions/Problem #6 to #10 Solutions.cpp
+++ 04 - Algorithms & Problem-Solving Level 1 Solutions/Problem #6 to #10 Solutions/Problem #6 to #10 Solutions/Problem #6 to #10 Solutions.cpp
@@ -60,7 +60,10 @@ float CalculateHalfName(short Number) {
 //proseger :PrintNumber
 //parameter:short Number,string Message
 //return :This is a proseger Prints Message and Number
-void PrintNumber(string Message,float Nuamber) {
+void PrintNumber(string Message,float Nuamber=0) {
+	if(Nuamber==0)
+		cout << Message << endl;
+	else
 	cout << Message +to_string(Nuamber) << endl;
 }
 
@@ -69,7 +72,7 @@ void PrintNumber(string Message,float Nuamber) {
 //return :This Function displays the Message and get a number's user
 short ReadNumber(string Message) {
 	short Number;
-	cout << Message << endl;
+	cout << Message ;
 	cin >> Number;
 	return Number;
 }
@@ -97,8 +100,8 @@ enPassFail enCheckMark(short Mark) {
 //prasager :PrintResults
 //parameter:short Mark
 //return :print Pass or Fail
-void PrintResults(short Mark) {
-	if (enCheckMark(Mark) == enPassFail::Pass)
+void PrintResults(enPassFail Mark) {
+	if (Mark == enPassFail::Pass)
 		cout << "You Passed";
 	else
 		cout << "You Faild";
@@ -124,32 +127,156 @@ short Sum(short num1, short num2, short num3) {
 float CalculaterAverage(short num1, short num2, short num3) {
 	return (float)Sum(num1, num2, num3) / 3;
 }
+
+//--------------------------------------------------------
+// #11
+// function:enCheckAverage
+//parameter:short TotalMark
+// return :this function check average is pass or not
+enPassFail enCheckAverage(short TotalMark)
+{
+	if (TotalMark < 50)
+		return enPassFail::Fail;
+	else
+		return enPassFail::Pass;
+}
+
+
+//--------------------------------------------------------
+// #12
+//function :MaxOf2Number
+//Parameter:short num1,short num2
+//return :it is chacke they numbers and give you largeNumber
+short MaxOf2Number(float num1, float num2) {
+	
+	 if (num2 == num1)
+		return num1;//equal
+	 else if (num1 > num2)
+		 return num1;
+	else
+		return num2;
+
+}
+
+//--------------------------------------------------------
+// #13
+//function :MaxOf3Number
+//Parameter:float num1,float num2,float num3
+//return :it is chacke they numbers and give you Max number
+short MaxOf3Number(float num1, float num2,float num3) {
+	return MaxOf2Number(MaxOf2Number(num1, num2),num3);
+}
+
+
+//--------------------------------------------------------
+// #14
+//proseager:SwapTwoNumbers
+//Parameters:float num1,float num2
+//Return: This is the code that can be used to switch between two numbers
+void SwapTwoNumbers(float &num1, float &num2) {
+	float swithc = num1;
+	num1 = num2;
+	num2 = swithc;
+	
+
+}
+
+//--------------------------------------------------------
+// #15
+//function:CalculateRectangArea
+//Parameters:float Lenth,float width
+//return :this function is can it find rectang area
+float CalculateRectangArea(float Lenth, float Width) {
+	return Lenth * Width; 
+}
+
+
 int main()
 {
 
 	// This line of code extracts the full name
-	//PrintFullName(GetFullName(stNameReadName()));
+	/*
+	PrintFullName(GetFullName(stNameReadName()));
+	*/
 
 	//this code Receives a number and gives you Hilf of that number
-	//float Number = ReadNumber("enter the number ?");
-	//PrintNumber("the half's Your number is :",CalculateHalfName(Number));
+	/*
+	float Number = ReadNumber("enter the number ?");
+	PrintNumber("the half's Your number is :",CalculateHalfName(Number));
+	*/
+	
 
 	//this code check the mark is pass or not 
-	//short Mark = ReadNumber("enter the Nubmer?");
-	//PrintResults(Mark);
+	/*
+	short Mark = ReadNumber("enter the Mark?");
+	PrintResults(enCheckMark(Mark));
+	*/
 
 	//this code sum three numbers
-   //short num1, num2, num3;
-	//num1 = ReadNumber("enter num1:");
-	//num2 = ReadNumber("enter num2:");
-	//num3 = ReadNumber("enter num3");
-	//PrintNumber("Total is : ", Sum(num1, num2, num3));
-
+	/*
+   short num1, num2, num3;
+	num1 = ReadNumber("enter num1:");
+	num2 = ReadNumber("enter num2:");
+	num3 = ReadNumber("enter num3");
+	PrintNumber("Total is : ", Sum(num1, num2, num3));
+	*/
 
 	//this code sum three numbers
+	/*
     short num1, num2, num3;
     num1 = ReadNumber("enter num1:");
     num2 = ReadNumber("enter num2:");
 	num3 = ReadNumber("enter num3");
 	PrintNumber("Average is : ", CalculaterAverage(num1, num2, num3));
+	*/
+
+	//this code check the Avrage is pass or not 
+	/*
+	short Avrage = ReadNumber("enter the Avrage?");
+	PrintResults(enCheckAverage(Avrage));
+	*/
+
+	//this code print the bigger number
+	/*
+	short num1, num2;
+	num1 = ReadNumber("enter a num1:");
+	num2 = ReadNumber("enter a num2:");
+	PrintNumber("Large numbe is :", MaxOf2Number(num1, num2));
+	*/
+
+	//this cod print the Max number between 3 numbers
+	/*
+	short num1, num2,num3;
+	num1 = ReadNumber("enter a num1:");
+	num2 = ReadNumber("enter a num2:");
+	num3 = ReadNumber("enter a num3:");
+	PrintNumber("Large numbe is :", MaxOf3Number(num1, num2,num3));
+	*/
+
+	//this code do be swap between two numbers
+	/*
+	float num1, num2;
+	num1 = ReadNumber("enter the num1:");
+	num2 = ReadNumber("enter the num2:");
+	
+	PrintNumber("they numbers beform swap");
+	PrintNumber("num1=", num1);
+	PrintNumber("num2=", num2);
+	
+	SwapTwoNumbers(num1, num2);
+	
+	PrintNumber("they numbers after swap");
+	PrintNumber("num1=", num1);
+	PrintNumber("num2=", num2);
+	*/
+
+
+	//this code calculate rctangl area
+	/*
+	float Lenth, Width;
+	Lenth = ReadNumber("enter the Lenth:");
+	Width = ReadNumber("enter the Width:");
+	
+	PrintNumber("the calculat rctangl area is :", CalculateRectangArea(Lenth, Width));
+	*/
 }
